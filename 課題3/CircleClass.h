ca26ae5a9bf58804ce369873856d293fc1fb8278
itotaiti <230481@jc-21.jp>
2024-07-10 11:37:03 +0900

    課題進め

--- 課題3/CircleClass.h
+++ 課題3/CircleClass.h
@@ -1,1 +1,12 @@
 #pragma once
+
+class CircleClass
+{
+	float area;
+	float r;
+
+public:
+	void Input();
+	void Calc();
+	void Disp();
+};
---
+++ 課題6/Calclation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"Calclation.h"
+
+void Calclation::SetA(float a)
+{
+	//足し算の方をやってセットAに入れる
+}
+
+void Calclation::SetB(float b)
+{
+	//引き算の方をやってセットBに入れる
+}
+
+void Calclation::Disp()
+{
+	//両方をcoutで出す
+}
